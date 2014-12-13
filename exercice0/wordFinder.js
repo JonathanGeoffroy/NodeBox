e50cd002c89353fa83a3576a83b2d605fc8cccd2
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2014-12-13 15:30:22 +0100

    remove solution code

--- exercice0/wordFinder.js
+++ exercice0/wordFinder.js
@@ -1,23 +1,6 @@
-var fs = require('fs');
-var _ = require('underscore');
-var async = require('async');
+// TODO: importer les modules requis
 
-var nbTotal = 0;
-var NUMBER_OF_FILES_PARSED_SIMULTANEOUSLY = 8;
 var wordToFind = /Module/g;
 var path = "./resources/";
 
-fs.readdir(path, function (err, filesInDir) {
-	async.eachLimit(filesInDir, NUMBER_OF_FILES_PARSED_SIMULTANEOUSLY,
-		function readFile(filename, asyncCallback) {
-			var nb = 0;
-			fs.readFile(path + filename, function (err, data) {
-				nb = data.toString().match(wordToFind).length;
-				nbTotal += nb;
-				console.log('nombre de ' + wordToFind + ' dans ' + filename + ' : ' + nb);
-				asyncCallback();
-			});
-		}, function onEnd() {
-			console.log('nombre de ' + wordToFind + ' dans le dossier : ' + nbTotal);
-		});
-});
+// TODO: implémenter l'exercice 0
