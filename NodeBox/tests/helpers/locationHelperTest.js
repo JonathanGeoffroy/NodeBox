7ff8b1b89c1a6d3325ccde2c00d297e0945a01a0
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2014-12-14 15:20:52 +0100

    run vows only one time; convert  decribes into batches

--- NodeBox/tests/helpers/locationHelperTest.js
+++ NodeBox/tests/helpers/locationHelperTest.js
@@ -5,7 +5,7 @@ var locationHelper = require(__dirname + "/../../helpers/locationHelper.js");
 var config = require(__dirname + "/../../config.js");
 
 // Create a Test Suite
-vows.describe('LocationHelper.getFileLocation').addBatch({
+vows.describe('LocationHelper').addBatch({
 	'when we search the FileLocation of root': {
 		topic: function () {
 			return locationHelper.getFileLocation('/');
@@ -36,10 +36,7 @@ vows.describe('LocationHelper.getFileLocation').addBatch({
 			}
 		}
 	}
-}).run(); // Run it
-
-
-vows.describe('LocationHelper.normalizeUrl').addBatch({
+}).addBatch({
 	'when we normalize empty url': {
 		topic: function () {
 			return locationHelper.normalizeUrl('');
@@ -76,9 +73,7 @@ vows.describe('LocationHelper.normalizeUrl').addBatch({
 			assert.equal(topic, '/first/second');
 		}
 	}
-}).run(); // Run it
-
-vows.describe('LocationHelper.isDirectory').addBatch({
+}).addBatch({
 	'when we call isDirectory with a directory': {
 		topic: function () {
 			var self = this;
@@ -103,9 +98,7 @@ vows.describe('LocationHelper.isDirectory').addBatch({
 			assert.equal(topic, false);
 		}
 	}
-}).run(); // Run it
-
-vows.describe('LocationHelper.getItemName').addBatch({
+}).addBatch({
 	'The item name of root': {
 		topic: function () {
 			return locationHelper.getItemName('/');
